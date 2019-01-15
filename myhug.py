674f89994c01ff6d279d98c6e524008a26a8a32b
jleatham <j.leatham@gmail.com>
2019-01-15 16:32:19 -0600

    webex test

--- myhug.py
+++ myhug.py
@@ -12,3 +12,11 @@ def happy_birthday(name: hug.types.text, age: hug.types.number, hug_timer=3):
 def echo(blah: hug.types.text,hug_timer=3):
     """Echos back whatever given"""
     return {'message': '{}'.format(blah), 'took': float(hug_timer)}
+
+@hug.get(examples='hello')
+def hello():
+    """Test for webex teams"""
+    roomId = 'Y2lzY29zcGFyazovL3VzL1JPT00vNjg2YTkwODAtZmZjMy0xMWU4LWI0NTgtMzc2MWQzZGY5MjNj'
+    msg = 'hello world'
+    return {"roomId": roomId,"markdown": msg}
+    
