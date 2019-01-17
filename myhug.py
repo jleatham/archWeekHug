8de14648bf8b8d18880290f429ad575f3107f1cb
jleatham <j.leatham@gmail.com>
2019-01-16 23:08:59 -0600

    markdown fix

--- myhug.py
+++ myhug.py
@@ -45,9 +45,9 @@ def hello(body):
             response = bot_post_to_room(room_id, command)
             print("botpost response: {}".format(response))
         else:
-            msg = ("Commands available: <spiff>,<news>,<promo>,<services>,<partner>,<capital>\n"
-                   "example: {bot} news\n"
-                   "example: {bot} spiff\n"
+            msg = ("Commands available: < spiff >,< news >,< promo >,< services >,< partner >,< capital >  \n"
+                   "example: {bot} news  \n"
+                   "example: {bot} spiff  \n"
                    ).format(bot = bot_name)
             response = bot_post_to_room(room_id, msg)
             print("botpost response: {}".format(response))            
