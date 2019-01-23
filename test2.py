128b827b60973e907d82e3265d66367d480c53c1
Minh <simxeon@gmail.com>
2019-01-22 20:51:03 -0600

    Update test2.py

---
+++ test2.py
@@ -0,0 +1,15 @@
+import test
+from secrets import SPARK_ACCESS_TOKEN, SMARTSHEET_TOKEN, HUGTEST_ROOM_ID
+
+"""
+1. Modify date to US format
+2. Purge past events from list
+
+User input
+<bot> TX
+-- output code block
+
+<bot> TX email
+-- generate email attachment --> method in test.py
+
+"""
