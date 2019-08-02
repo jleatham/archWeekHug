b845b942b4c4aeb8586a6d8f14a5a3a9f5ff6e08
Minh <simxeon@gmail.com>
2019-08-02 13:42:16 -0500

    Update myhug.py

--- myhug.py
+++ myhug.py
@@ -228,8 +228,19 @@ def communicate_to_user(ss_client,room_id,headers,bot_name,data,state_filter,arc
     if not help:
         if not mobile_filter:
             state_list_joined = " ".join(state_filter)
-            msg = format_code_print_for_bot(data,state_list_joined,CODE_PRINT_COLUMNS)
-            response = bot_post_to_room(room_id, msg, headers)
+            if len(data) >=50:
+                sub_data_a = data[:40]
+                msg = format_code_print_for_bot(sub_data_a,state_list_joined,CODE_PRINT_COLUMNS)
+                response = bot_post_to_room(room_id, msg, headers)
+                sub_data_a = data[40:]
+                msg = format_code_print_for_bot(sub_data_b,state_list_joined,CODE_PRINT_COLUMNS)
+                response = bot_post_to_room(room_id, msg, headers)
+            else:
+                msg = format_code_print_for_bot(data,state_list_joined,CODE_PRINT_COLUMNS)
+                response = bot_post_to_room(room_id, msg, headers)
+
+ #           msg = format_code_print_for_bot(data,state_list_joined,CODE_PRINT_COLUMNS)
+ #           response = bot_post_to_room(room_id, msg, headers)
             msg = generate_html_table_for_bot(data,state_list_joined,EMAIL_COLUMNS)
             email_filename = generate_email(msg)
             response = bot_send_email(room_id,email_filename)  
