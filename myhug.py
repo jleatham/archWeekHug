9fb74983026a8926e38e417bc96d5fc93cd28fae
Minh <simxeon@gmail.com>
2019-09-06 11:17:34 -0500

    Update myhug.py

--- myhug.py
+++ myhug.py
@@ -665,6 +665,7 @@ def communicate_to_user(ss_client,room_id,headers,bot_name,data,state_filter,hel
         msg = generate_html_table_for_bot(data,state_list_joined,EMAIL_COLUMNS)
         email_filename = generate_email(msg)
         response = bot_send_email(room_id,email_filename)  
+        msg_ids_list.append(response["id"])
         
         response = bot_post_to_room(room_id, f"Have an event to add?  Please email the event **[HERE](mailto:{os.environ['EMAIL_ADD_EVENT']})**", headers)
         msg_ids_list.append(response["id"])        
